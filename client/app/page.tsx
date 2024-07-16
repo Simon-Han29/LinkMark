3cad61afb4e52bdd9aad05a0810cd1d5a9414464
Simon-Han29 <68207476+Simon-Han29@users.noreply.github.com>
2024-07-16 13:36:25 -0400

    folder structure + addlink, but still broken

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,37 +1,55 @@
-'use client'
-import {useState, useEffect, ChangeEvent} from "react"
-import { useAuth } from "@/context/AuthContext";
-import Navbar from "@/components/Navbar"
-import Image from "next/image"
+'use client';
+import { useState, useEffect, ChangeEvent } from 'react';
+import { useAuth } from '@/context/AuthContext';
+import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function Home() {
-  const {links, username, isAuthenticated, addLink, deleteLink} = useAuth();
-  const [newLink, setNewLink] = useState<string>("")
-  const [newLinkName, setNewLinkName] = useState<string>("")
-  const [isMenuShowing, setIsMenuShowing] = useState<boolean>(false)
+  const { folders, username, isAuthenticated, addLink, deleteLink } = useAuth();
+  const [newLink, setNewLink] = useState<string>('');
+  const [newLinkName, setNewLinkName] = useState<string>('');
+  const [isMenuShowing, setIsMenuShowing] = useState<boolean>(false);
+  const [selectedFolder, setSelectedFolder] = useState<string>('');
+  const [selectedFolderId, setSelectedFolderId] = useState<string>('');
+  const [displayedLinks, setDisplayedLinks] = useState<Object>({});
+
   const handleNewLinkChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewLink(event.target.value.trim())
-  }
+    setNewLink(event.target.value.trim());
+  };
+
   const handleNewLinkNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewLinkName(event.target.value.trim())
-  }
+    setNewLinkName(event.target.value.trim());
+  };
+
+  const selectFolder = (folderName: string, folderId: string, links: Object) => {
+    setSelectedFolder(folderName);
+    setSelectedFolderId(folderId);
+    setDisplayedLinks(links);
+  };
 
   const toggleMenu = () => {
-    if(isMenuShowing) {
-      setIsMenuShowing(false)
-    } else {
-      setIsMenuShowing(true)
+    setIsMenuShowing(!isMenuShowing);
+  };
+
+  const handleAddLink = async () => {
+    await addLink(newLink, newLinkName, selectedFolderId);
+    // Update displayedLinks after adding a new link
+    const updatedFolder = folders.find(folder => folder.fid === selectedFolderId);
+    if (updatedFolder) {
+      setDisplayedLinks(updatedFolder.links);
     }
-  }
+  };
+
   return (
     <div className="text-white">
-      <Navbar/>
+      <Navbar />
       {isAuthenticated ? (
         <div>
-          <div className={isMenuShowing ? "opacity-50": ""}>
-            <div className={`flex justify-center p-10`}>
-              <Image src="/default-monochrome.svg" alt="" width={400} height={50}/>
+          <div className={isMenuShowing ? 'opacity-50' : ''}>
+            <div className="flex justify-center p-10">
+              <Image src="/default-monochrome.svg" alt="" width={400} height={50} />
             </div>
             <div className="flex justify-center mt-8 mb-8">
               <h1>{`Welcome back, ${username}!`}</h1>
@@ -42,39 +60,61 @@ export default function Home() {
             <div className="flex">
               <div className="bg-neutral-900 ml-5 p-10 rounded-[20px] w-[30%]">
                 <p>Folders</p>
-              </div>
-              <div className="bg-neutral-900 mx-5 p-10 rounded-[20px] w-[70%]">
-                <div className="flex justify-end">
-                  <button onClick={toggleMenu} className="bg-violet-600 h-10 w-40 rounded-[5px]">+ New</button>
-                </div>
-                
-                {Object.entries(links).map(([key, data]) => (
-                  <div key={key}>
-                    <p>{`${data.linkname}:`}</p><a href={data.link}>{data.link}</a>
-                    <button onClick={() => deleteLink(key)}>Delete</button>
+                {folders.map((folder) => (
+                  <div key={folder.fid}>
+                    <p onClick={() => selectFolder(folder.name, folder.fid, folder.links)}>{folder.name}</p>
                   </div>
                 ))}
+              </div>
+              <div className="bg-neutral-900 mx-5 p-10 rounded-[20px] w-[70%] flex">
+                <div className="w-[80%] flex items-center">
+                  <p>{`/${selectedFolder}`}</p>
+                </div>
+                <div className="flex justify-end w-[20%]">
+                  <button onClick={toggleMenu} className="bg-violet-600 h-10 w-40 rounded-[5px]">
+                    + New Link
+                  </button>
+                  <div>
+                    {displayedLinks &&
+                      Object.values(displayedLinks).map((data) => (
+                        <div key={data.linkId}>
+                          <p>{data.link}</p>
+                        </div>
+                      ))}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           {isMenuShowing ? (
-              <div className="bg-neutral-800 absolute left-[50%] top-[50%] h-[300px] w-[500px] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
-                <div className="flex flex-col h-56 w-96 justify-center items-center">
-                  <input type="text" placeholder="Paste Link here" onChange={handleNewLinkChange} className="text-black rounded-[5px] px-5 mb-5 h-10"/>
-                  <input type="text" placeholder="Link Name" onChange={handleNewLinkNameChange} className="text-black rounded-[5px] px-5 mb-5 h-10"/>
-                  <button onClick={() => addLink(newLink, newLinkName)} className="bg-violet-900 h-10 w-28 rounded-[5px]">Add</button>
-                </div>
+            <div className="bg-neutral-800 absolute left-[50%] top-[50%] h-[300px] w-[500px] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
+              <div className="flex flex-col h-56 w-96 justify-center items-center">
+                <input
+                  type="text"
+                  placeholder="Paste Link here"
+                  onChange={handleNewLinkChange}
+                  className="text-black rounded-[5px] px-5 mb-5 h-10"
+                />
+                <input
+                  type="text"
+                  placeholder="Link Name"
+                  onChange={handleNewLinkNameChange}
+                  className="text-black rounded-[5px] px-5 mb-5 h-10"
+                />
+                <button onClick={handleAddLink} className="bg-violet-900 h-10 w-28 rounded-[5px]">
+                  Add
+                </button>
               </div>
-            ):(<></>)}
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
-
-      ):(
+      ) : (
         <div>
           <h1>Login to start saving links</h1>
         </div>
-        
       )}
-      
     </div>
-  )
+  );
 }
--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ const authenticateToken = (req, res, next) => {
   })
 }
 
+
 app.post("/api/signup", async(req, res) => {
   const {username, password} = req.body;
   try {
@@ -50,13 +51,34 @@ app.post("/api/signup", async(req, res) => {
       }
   
       const hashedPassword = await bcrypt.hash(password, 10)
-      const registerUserQuery = `
-        INSERT INTO users (uid, username, password, links)
-        VALUES ($1, $2, $3, $4)
+
+      //create default folder
+      //check give an id to the folder, make sure it does not exist
+      const defaultFid = generateRandomId(10);
+
+      const findFolderQuery = `
+        SELECT * FROM folders
+        WHERE fid=$1
       `
-  
-      await db.query(registerUserQuery, [uid, username, hashedPassword, "{}"])
-      res.status(201).json({msg:"User registered successfully"}) 
+      const folderFound = await db.query(findFolderQuery, [defaultFid])
+      while (folderFound.rowCount !== 0) {
+        defaultFid = generateRandomId(10)
+        folderFound = await db.query(findFolderQuery, [defaultFid])
+      }
+
+
+      const registerUserQuery = `
+        INSERT INTO users (uid, username, password)
+        VALUES ($1, $2, $3)
+      `
+      const initDefaultFolder = `
+        INSERT INTO folders (fid, uid, parentid, name, links)
+        VALUES ($1, $2, $3, $4, $5)
+      `
+      await db.query(registerUserQuery, [uid, username, hashedPassword])
+      await db.query(initDefaultFolder, [defaultFid, uid, null, "Default", "{}"])
+      
+      res.status(201).json({msg:"User registered successfully and default folder created"}) 
     }
   } catch(err) {
     console.log(err)
@@ -82,55 +104,105 @@ app.post("/api/login", async (req, res) => {
     console.log(matchResult)
     if (matchResult) {
 
-      const getLinksQuery = `
-        SELECT links from users
+      const getUID = `
+        SELECT uid from users
         WHERE username=$1
       `
-      let linksRes = await db.query(getLinksQuery, [username])
-      let links = linksRes.rows[0].links
 
+      const uidRes = await db.query(getUID, [username])
+      const uid = uidRes.rows[0].uid;
+
+      const getFoldersQuery = `
+        SELECT * FROM folders
+        WHERE uid=$1
+      `
+
+      const folderRes = await db.query(getFoldersQuery, [uid])
+      console.log("Folder res")
+      console.log(folderRes);
+      const folders = folderRes.rows;
 
       let data = {
         "username": username,
-        "links": links,
+        "uid": uid,
+        "folders": folders
       }
+
       const token = jwt.sign(data, jwtSecretKey)
       res.status(201).json({msg:"Login Successful", token})
+      // res.status(201).json({msg: "Test success"})
     } else {
       res.status(409).json({msg: "Incorrect Password"})
     }
   }
 })
 
-app.post("/api/links", authenticateToken, async (req, res) => {
+app.get("/folders", authenticateToken,  async(req, res) => {
   try {
-    const username = req.body.username;
-    const newLink = req.body.link;
-    const newLinkName = req.body.linkName;
-    const getLinksQuery = `
-      SELECT links FROM users
+    const username = req.params.username;
+    const getUID = `
+      SELECT uid from users
       WHERE username=$1
     `
-    let updatedLinks = await db.query(getLinksQuery, [username])
-    updatedLinks = updatedLinks.rows[0].links
-    let newLinkId = generateRandomId(10)
-    while (updatedLinks[newLinkId] != undefined) {
-      linkId = generateRandomId(10)
-    }
-    updatedLinks[newLinkId] = {
-      "link": newLink,
-      "linkname": newLinkName
-    }
+    const uidRes = await db.query(getUID, [username])
+    const uid = uidRes.rows[0].uid;
+    const getFoldersQuery = `
+      SELECT * FROM folders
+      WHERE uid=$1
+    `
+    const folderRes = await db.query(getFoldersQuery, [uid])
+    const folders = folderRes.rows;
+    res.status(201).json({"folders":folders})
+  } catch(err) {
+    res.status(500).send();
+  }
 
-    const updateLinksQuery = `
-      UPDATE users
-      SET links=$1
-      WHERE username=$2
+})
+
+app.post("/api/links", authenticateToken, async (req, res) => {
+  try {
+    const newLink = req.body.link;
+    const newLinkName = req.body.linkName;
+    const fid = req.body.fid
+    const uid = req.body.uid
+    const getFolderQuery = `
+      SELECT links from folders
+      WHERE fid=$1
     `
 
-    await db.query(updateLinksQuery, [updatedLinks, username])
-    res.status(201).json(updatedLinks);
+    const getFolderRes = await db.query(getFolderQuery, [fid])
+    let updatedFolder = getFolderRes.rows[0].links
+
+    let newLinkId = generateRandomId(10)
+    while (updatedFolder[newLinkId] != undefined) {
+      newLinkId = generateRandomId(10)
+    }
+    
+    updatedFolder[newLinkId] = {
+      "link": newLink,
+      "linkname": newLinkName,
+      "linkId": newLinkId
+    }
+    
+    const updateFolderQuery = `
+      UPDATE folders
+      SET links=$1
+      WHERE fid=$2
+    `
+
+    await db.query(updateFolderQuery, [updatedFolder, fid])
+
+    const userFolders = `
+      SELECT * FROM folders
+      WHERE uid=$1
+    `
+
+
+    const userFoldersRes = await db.query(userFolders, [uid])
+    console.log(userFoldersRes.rows)
+    res.status(201).json({"folders":userFoldersRes.rows});
   } catch(err) {
+    console.log(err)
     res.status(500).send(); 
   }
 
@@ -158,6 +230,34 @@ app.delete("/api/links", authenticateToken, async(req, res) => {
 
   } catch(err) {
     res.status(500).send();
+  }
+})
+
+app.post("/api/refresh", authenticateToken, async(req, res) => {
+  try {
+    const uid = req.body.uid
+    const username = req.body.username
+
+    const getFoldersQuery = `
+      SELECT * FROM folders
+      WHERE uid=$1
+    `
+
+    const folderRes = await db.query(getFoldersQuery, [uid])
+    console.log("Folder res")
+    console.log(folderRes);
+    const folders = folderRes.rows;
+
+    let data = {
+      "username": username,
+      "uid": uid,
+      "folders": folders
+    }
+
+    const token = jwt.sign(data, jwtSecretKey)
+    res.status(201).json({msg:"Login Successful", token})
+  } catch(err) {
+
   }
 })
 
